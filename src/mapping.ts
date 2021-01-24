921a6bd04026d533e004822f2ef36fffaa80e6c4
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2021-01-23 22:39:23 -0800

    tweaking

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,11 +1,11 @@
 import { nucleotides, features, vaccine } from "./vaccine";
 
 const mapping = {
-  C: "C4",
-  G: "G4",
-  A: "A4",
+  C: { pitch: "C4", channel: 1 },
+  G: { pitch: "G3", channel: 2 },
+  A: { pitch: "A3", channel: 3 },
   // hi-hat:
-  Ψ: "G#2"
+  Ψ: { pitch: "G#2", channel: 4 }
 };
 
 type Nucleotides = keyof typeof mapping;
@@ -15,15 +15,13 @@ export const mapped = nucleotides.map((nucleotide, index) => {
     features.find(([, beforeIndex]) => index < beforeIndex) ||
     ([vaccine.length, vaccine.length, "end", false] as const);
   const indexWithinFeature = index - featureStartIndex;
-  const pitch = mapping[nucleotide as Nucleotides];
-  const channel = nucleotide === "Ψ" ? 2 : 1;
+  const mapped = mapping[nucleotide as Nucleotides];
   const groupBy = codons ? 3 : 4;
   const beat = indexWithinFeature % groupBy === 0;
   return {
-    pitch,
-    channel,
-    duration: codons ? "8t" : "8",
-    velocity: beat ? 70 : 50,
+    ...mapped,
+    duration: codons ? "8t" : index >= 3879 ? "16" : index < 2 ? "2" : "8",
+    velocity: beat ? 80 : 50,
     marker: featureStartIndex === index ? feature : undefined
   };
 });

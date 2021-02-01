0662f769f47e5f31c9a1cd5e167482d113d36b32
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2021-02-01 09:50:35 -0800

    prettier and yarn

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import MidiWriter from "midi-writer-js";
-import { mapped } from "./mapping";
+import MidiWriter from 'midi-writer-js'
+import {mapped} from './mapping'
 
-const track = new MidiWriter.Track();
+const track = new MidiWriter.Track()
 // Define an instrument (optional):
-track.addEvent(new MidiWriter.ProgramChangeEvent({ instrument: 1 }));
+track.addEvent(new MidiWriter.ProgramChangeEvent({instrument: 1}))
 
-mapped.forEach(({ marker, ...event }) => {
-  if (marker) track.addMarker(marker);
-  track.addEvent(new MidiWriter.NoteEvent(event));
-});
+mapped.forEach(({marker, ...event}) => {
+  if (marker) track.addMarker(marker)
+  track.addEvent(new MidiWriter.NoteEvent(event))
+})
 
-const writer = new MidiWriter.Writer(track);
-const uri = writer.dataUri();
+const writer = new MidiWriter.Writer(track)
+const uri = writer.dataUri()
 
-document.getElementById("app")!.innerHTML = `
+document.getElementById('app')!.innerHTML = `
 <h1>Welcome to SARS-CoV-2 vaccine to MIDI converter</h1>
 <div>
   Download your MIDI
   <a href="${uri}">here</a>.
 </div>
-`;
+`
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,27 +1,27 @@
-import { nucleotides, features, vaccine } from "./vaccine";
+import {nucleotides, features, vaccine} from './vaccine'
 
 const mapping = {
-  C: { pitch: "C4", channel: 1 },
-  G: { pitch: "G3", channel: 2 },
-  A: { pitch: "A3", channel: 3 },
+  C: {pitch: 'C4', channel: 1},
+  G: {pitch: 'G3', channel: 2},
+  A: {pitch: 'A3', channel: 3},
   // hi-hat:
-  Ψ: { pitch: "G#2", channel: 4 }
-};
+  Ψ: {pitch: 'G#2', channel: 4},
+} as const
 
-type Nucleotides = keyof typeof mapping;
+type Nucleotides = keyof typeof mapping
 
 export const mapped = nucleotides.map((nucleotide, index) => {
   const [featureStartIndex, nextFeatureStartIndex, feature, codons] =
     features.find(([, beforeIndex]) => index < beforeIndex) ||
-    ([vaccine.length, vaccine.length, "end", false] as const);
-  const indexWithinFeature = index - featureStartIndex;
-  const mapped = mapping[nucleotide as Nucleotides];
-  const groupBy = codons ? 3 : 4;
-  const beat = indexWithinFeature % groupBy === 0;
+    ([vaccine.length, vaccine.length, 'end', false] as const)
+  const indexWithinFeature = index - featureStartIndex
+  const mapped = mapping[nucleotide as Nucleotides]
+  const groupBy = codons ? 3 : 4
+  const beat = indexWithinFeature % groupBy === 0
   return {
     ...mapped,
-    duration: codons ? "8t" : index >= 3879 ? "16" : index < 2 ? "2" : "8",
+    duration: codons ? '8t' : index >= 3879 ? '16' : index < 2 ? '2' : '8',
     velocity: beat ? 80 : 50,
-    marker: featureStartIndex === index ? feature : undefined
-  };
-});
+    marker: featureStartIndex === index ? feature : undefined,
+  } as const
+})
